32fc7fe25dbe0ca44830c6ce9cb42e3c92774a0b
Unknown <nmitafidis@gmail.com>
2019-09-14 05:41:19 +0300

    Properties

--- start_service.py
+++ start_service.py
@@ -2,8 +2,9 @@ import subprocess
 import os
 
 commands = [
+    'sudo -s',
     'docker rm -f $(docker ps -a -q)',
-    'cd DockerKafka && docker-compose up -d', 
+    'cd DockerKafka && docker-compose -f zk-single-kafka-single.yml up', 
     'cd DockerSpark && docker-compose up -d',
     'docker run -d -p 27017-27019:27017-27019 --name mongodb mongo:latest'
     ]
